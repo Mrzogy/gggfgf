7deab206a6185fcab1a0542136335e2262acc49c
mrzogy <magbolmrzogy@gmail.com>
2023-05-05 04:30:48 +0300

    Initial Commit

---
+++ gggfg/gggfgApp.swift
@@ -0,0 +1,17 @@
+//
+//  gggfgApp.swift
+//  gggfg
+//
+//  Created by 3bood on 15/10/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct gggfgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
